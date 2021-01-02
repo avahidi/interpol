e3b1b795fe7903c10669847af84e2764f6132bf2
Arash Vahidi <arash.vahidi@gmail.com>
2021-01-02 20:29:38 +0100

    Why a simple tool when you can overengineer it?
    added better error handling
    added better status reporting
    minor refactoring

--- examples/webpass/README.rst
+++ examples/webpass/README.rst
@@ -2,4 +2,4 @@
 
     webpass --negtext "Invalid" -username "{{file filename=usernames.txt}}" -password "{{file filename=passwords.txt}}" -url "http://localhost/test/login"                                           
 
-It started as an experiment at work, and I felt it would do a nice example on real-world use of Interpol.
+It started as an experiment at work, and I felt it would do a nice example on real-world use of Interpol. 
--- examples/webpass/webpass.go
+++ examples/webpass/webpass.go
@@ -15,33 +15,74 @@ import (
 	"bitbucket.org/vahidi/interpol"
 )
 
-// parameters
-var threads = flag.Int("threads", 10, "Number of threas")
-var url = flag.String("url", "http://localhost/", "Target URL")
-var userdesc = flag.String("username", "", "Username expression, for example {{file filename=usernames.txt}}")
-var passdesc = flag.String("password", "", "Password expression")
-var postext = flag.String("postext", "", "Text to check for positive match")
-var negtext = flag.String("negtext", "", "Text to check for positive match")
-
-// error messages
-func failOnce(url string, count, max int, err error) {
-	fmt.Fprintf(os.Stderr, "failure %d/%d (%v)...\n", count, max, err)
+// data for one web access, change this to change the application behavior
+type access struct {
+	username string
+	password string
 }
 
-func failFatal(url string, err error) {
-	fmt.Fprintf(os.Stderr, "Final failure %v\n", err)
-	os.Exit(20)
+func newAccess(iss []*interpol.InterpolatedString) *access {
+	return &access{username: iss[0].String(), password: iss[1].String()}
+}
+
+func (s access) values() URL.Values {
+	return URL.Values{"username": {s.username}, "password": {s.password}}
+}
+
+func (s access) String() string {
+	return fmt.Sprintf("'%s'/'%s'", s.username, s.password)
+}
+
+// stats
+type stats struct {
+	start time.Time
+	count int
+}
+
+func (s *stats) time() string {
+	return fmt.Sprintf("%0.2f/s", float32(time.Now().Sub(s.start))/float32(s.count))
+}
+
+func (s *stats) report(a *access) {
+	s.count++
+	if s.count == 1 {
+		s.start = time.Now()
+	} else if s.count == 10 || s.count == 100 || (s.count%1000) == 0 {
+		fmt.Fprintf(os.Stderr, "%8d: %v (%s)...      \r", s.count, a, s.time())
+	}
+}
+
+func (s *stats) finish() {
+	fmt.Printf("\n\nDONE with %d attempts (%s)\n\n", s.count, s.time())
+}
+
+// error messages
+func failFatal(err error, format string, args ...interface{}) {
+	msg := fmt.Sprintf(format, args...)
+	log.Fatalf("%s (%v)\n", msg, err)
+}
+
+func failOnce(err error, count, max int, format string, args ...interface{}) {
+	if count >= max {
+		failFatal(err, format, args...)
+	} else {
+		msg := fmt.Sprintf(format, args...)
+		log.Printf("%s -- attempt %d/%d failed (%v)...\n", msg, count, max, err)
+	}
 }
 
 // networking
 func post(url string, args URL.Values) ([]byte, error) {
 	c := &http.Client{}
-
 	resp, err := c.PostForm(url, args)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode/100 != 2 {
+		return nil, fmt.Errorf("Failed with HTTP code %s", resp.Status)
+	}
 
 	return ioutil.ReadAll(resp.Body)
 }
@@ -53,11 +94,11 @@ func tryPost(url string, args URL.Values, tries int) ([]byte, error) {
 		if err == nil {
 			return data, err
 		}
-		failOnce(url, i+1, tries, err)
+		failOnce(err, i+1, tries, url)
 		time.Sleep(time.Duration((1 + 3*i)) * time.Second)
 	}
 
-	failFatal(url, err)
+	failFatal(err, url)
 	return nil, err
 }
 
@@ -73,69 +114,76 @@ func (s search) verify(text string) bool {
 		(s.postext != "" && strings.Contains(string(text), s.postext))
 }
 
-func (s search) report(username, password string) {
-	fmt.Printf("FOUND '%s'/'%s'\n", username, password)
+func (s search) report(a *access) {
+	fmt.Printf("\n\nFOUND '%v' \n\n", a)
 	os.Exit(0)
 }
 
 // worker
-type job struct {
-	username string
-	password string
-}
-
-func worker(s search, wg sync.WaitGroup, jobs <-chan job) {
+func worker(id int, s search, wg *sync.WaitGroup, jobs <-chan *access) {
 	defer wg.Done()
 
 	for job := range jobs {
-		args := URL.Values{"username": {job.username}, "password": {job.password}}
+		args := job.values()
 		resp, err := tryPost(s.url, args, 5)
 		if err != nil {
-			fmt.Printf("FAILED on %v: %v\n", job, err)
-			os.Exit(3)
+			failFatal(err, "Thread %d failed on %v", job)
 		}
 
 		text := string(resp)
 		if s.verify(text) {
-			s.report(job.username, job.password)
+			s.report(job)
 			return
 		}
 	}
 }
 
+// parameters
+var (
+	threads  = flag.Int("threads", 10, "Number of threas")
+	url      = flag.String("url", "http://localhost/", "Target URL")
+	userdesc = flag.String("username", "", "Username expression, for example {{file filename=usernames.txt}}")
+	passdesc = flag.String("password", "", "Password expression")
+	postext  = flag.String("postext", "", "Text to check for positive match")
+	negtext  = flag.String("negtext", "", "Text to check for positive match")
+)
+
 func main() {
 	flag.Parse()
 	if flag.NArg() != 0 || *userdesc == "" || *passdesc == "" || (*postext == "" && *negtext == "") {
-		fmt.Fprintf(os.Stderr, "Usage: %s -url <url> -username <expression> -password <expression> -postext/-negtext <text>\n", os.Args[0])
+		if *userdesc == "" || *passdesc == "" {
+			fmt.Fprintf(os.Stderr, "Missing username or password expressions\n")
+		}
+
+		if *postext == "" && *negtext == "" {
+			fmt.Fprintf(os.Stderr, "Missing both positive and negative match text\n")
+		}
+		fmt.Fprintf(os.Stderr, "Usage example:\n%s -url http://localhost/login -username user -password '{{file filename=passwords.txt}}' -postext 'success'\n", os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(3)
 	}
 
-	s := search{url: *url, postext: *postext, negtext: *negtext}
+	search := search{url: *url, postext: *postext, negtext: *negtext}
 
 	ip := interpol.New()
 	vals, err := ip.AddMultiple(*userdesc, *passdesc)
 	if err != nil {
-		log.Fatalf("Bad things just happened: %v", err)
+		failFatal(err, "Error in interpol expressions")
 	}
 
 	var wg sync.WaitGroup
-	jobs := make(chan job, *threads)
-	for w := 1; w < *threads; w++ {
+	jobs := make(chan *access, *threads)
+	for w := 0; w < *threads; w++ {
 		wg.Add(1)
-		go worker(s, wg, jobs)
+		go worker(w, search, &wg, jobs)
 	}
 
-	i := 0
+	stat := stats{}
+	defer stat.finish()
 	for ip.Next() {
-		username := vals[0].String()
-		password := vals[1].String()
-		jobs <- job{username: username, password: password}
-
-		i = i + 1
-		if (i % 1000) == 0 {
-			fmt.Printf("%d: %s %s...          \r", i, username, password)
-		}
+		access := newAccess(vals)
+		jobs <- access
+		stat.report(access)
 	}
 
 	close(jobs)
